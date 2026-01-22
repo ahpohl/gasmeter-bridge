17b15da8d78d9022fd0e0f670aef6c3fb817d849
Alexander Pohl <alex@ahpohl.com>
2026-01-22 20:54:55 +0100

    project skeleton files

--- resources/firmware/include/util.h
+++
@@ -1,9 +0,0 @@
-#ifndef UTIL_H
-#define UTIL_H
-
-void SendBuffer(const unsigned char *input, size_t len);
-void SendValue(uint32_t value);
-void SendRaw(int16_t raw, int32_t counts);
-uint16_t Crc16(const uint8_t *packet, size_t length);
-
-#endif // UTIL_H
--- resources/postgres/values.sql
+++
@@ -1,7 +0,0 @@
-\c gasmeter
-
-INSERT INTO sensors(id, serial_num, customer_id, part_num, mfg_date) VALUES
-(1, '42010646', '20141182555', 'G4RF1c', '2020');
-
-INSERT INTO plan(id, price, rate, factor) VALUES
-(1, 0.0583, 261.80, 10.835);
