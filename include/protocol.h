91d6f6138d6c67d60384171930d638efa9778fd8
Alexander Pohl <alex@ahpohl.com>
2026-01-24 21:32:51 +0100

    added protocol data structures

---
+++ include/protocol.h
@@ -0,0 +1,57 @@
+#ifndef PROTOCOL_H
+#define PROTOCOL_H
+
+#include <string>
+
+namespace Protocol {
+
+enum class Status : unsigned char {
+  OK = 0x00,
+  UartNoData = 0x01,
+  UartBufferOverflow = 0x02,
+  UartParityError = 0x04,
+  UartOverrunError = 0x08,
+  UartFrameError = 0x10,
+  UartCrcError = 0x20,
+  CommandNotImplemented = 0x40,
+  VariableNotExist = 0x80
+};
+
+enum class Command : unsigned char {
+  ClearMeterVolume = 1,
+  SetMeterVolume = 2,
+  SetThresholds = 3,
+  MeasureRequestDsp = 4
+};
+
+enum class DspValue : unsigned char { GasVolume = 1, RawIr = 2 };
+
+// Helper functions
+inline std::string statusToString(Status status) {
+  switch (status) {
+  case Status::OK:
+    return "Everything is OK";
+  case Status::UartNoData:
+    return "UART no data";
+  case Status::UartBufferOverflow:
+    return "UART buffer overflow";
+  case Status::UartParityError:
+    return "UART parity error";
+  case Status::UartOverrunError:
+    return "UART overrun error";
+  case Status::UartFrameError:
+    return "UART frame error";
+  case Status::UartCrcError:
+    return "UART CRC error";
+  case Status::CommandNotImplemented:
+    return "Command is not implemented";
+  case Status::VariableNotExist:
+    return "Variable does not exist";
+  default:
+    return "Unknown";
+  }
+}
+
+} // namespace Protocol
+
+#endif /* PROTOCOL_H */
